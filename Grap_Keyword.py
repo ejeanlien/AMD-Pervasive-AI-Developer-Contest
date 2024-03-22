586d6d59a4ec0181422b9091191929522176d0d8
ejeanlien <ejean.lien@gmail.com>
2024-03-22 19:04:49 +0800

    Grap_Keyword.py

--- Grap_Keyword.py
+++ Grap_Keyword.py
@@ -1,15 +1,16 @@
 import json
 
-def get_keyword_value(json_file, keyword):
+def get_keyword_value_and_id(json_file, keyword):
     with open(json_file, 'r') as f:
         data = json.load(f)
         if keyword in data:
-            return data[keyword]
+            return {'value': data[keyword], 'id': data.get('id', None)}
         else:
             return None
 
 # Usage
 json_file = 'your_file.json'  # replace with your json file
 keyword = 'your_keyword'  # replace with your keyword
-value = get_keyword_value(json_file, keyword)
-print(f'The value of {keyword} is {value}')
+result = get_keyword_value_and_id(json_file, keyword)
+print(f'The value of {keyword} is {result["value"]} and its related ID is {result["id"]}')
+
